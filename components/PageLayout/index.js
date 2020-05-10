d1c72df7b61193340359c47ad75d66a8142c6ea4
Shivay Lamba <shivaylamba@gmail.com>
2020-05-10 23:19:54 +0530

    added footer

--- components/PageLayout/index.js
+++ components/PageLayout/index.js
@@ -2,12 +2,14 @@ import React from "react";
 import { ThemeProvider } from "styled-components";
 import theme from "theme/index";
 import Navbar from "components/Navbar";
+import Footer1 from "components/Footer";
 
 function Page({ children }) {
   return (
     <ThemeProvider theme={theme}>
       <Navbar />
       {children}
+      <Footer1 />
     </ThemeProvider>
   );
 }
